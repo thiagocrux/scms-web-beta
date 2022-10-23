b67d62dfab5a107913b93b27b602c10d742b4440
Thiago Cruz <thiagocruz.eu@gmail.com>
2022-10-23 20:26:49 -0300

    chore: config styled-components

---
+++ src/styles/globals.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 1.6rem;
+  }
+`;
+
+export default GlobalStyle;
